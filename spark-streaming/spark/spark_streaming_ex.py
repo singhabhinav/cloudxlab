00bc21190b5d44e438cf0c510a1cfd6b046ca0dc
Abhinav <abhinav.foss@gmail.com>
2016-07-13 11:52:08 +0000

    Update spark streaming code
    We are processing order status in spark streaming now instead of counting "yes"

--- spark-streaming/spark/spark_streaming_ex.py
+++ spark-streaming/spark/spark_streaming_ex.py
@@ -6,30 +6,22 @@ import json
 import sys
 import pprint
 
-def putDataToKafka(iter1):
-	client = KafkaClient(hosts="ip-172-31-13-154.ec2.internal:6667")
-	topic = client.topics['one-mindata']
-	for sr in iter1:
-		with topic.get_producer() as producer:
-			producer.produce(json.dumps(sr))
-
-def putDataToKafka1(iter1):
-	client = KafkaClient(hosts="ip-172-31-13-154.ec2.internal:6667")
-	topic = client.topics['one-mindata']
-	with topic.get_producer() as producer:
-		for sr in iter1:
-			sr.pprint()
-			producer.produce(json.dumps(sr))
-
+def pushDataInKafka(iter1):
+    client = KafkaClient(hosts="ip-172-31-13-154.ec2.internal:6667")
+    topic = client.topics['one-mindata']
+    for sr in iter1:
+	    with topic.get_producer() as producer:
+		    producer.produce(json.dumps(sr))
 
 zkQuorum, topic = sys.argv[1:]
-sc = SparkContext(appName="KafkaWordCount")
-ssc = StreamingContext(sc, 1)
+sc = SparkContext(appName="KafkaOrderCount")
+ssc = StreamingContext(sc, 10)
 kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
 lines = kvs.map(lambda x: x[1])
-words = lines.flatMap(lambda line: line.split(" "))
-pairs = words.map(lambda word: (word, 1))
-wordCounts = pairs.reduceByKey(lambda x, y: x + y)
-wordCounts.foreachRDD(lambda rdd: rdd.foreachPartition(putDataToKafka))
+status_count = lines.map(lambda line: line.split(",")[2]) \
+              .map(lambda order_status: (order_status, 1)) \
+              .reduceByKey(lambda a, b: a+b)
+status_count.pprint()
+status_count.foreachRDD(lambda rdd: rdd.foreachPartition(pushDataInKafka))
 ssc.start()
 ssc.awaitTermination()
