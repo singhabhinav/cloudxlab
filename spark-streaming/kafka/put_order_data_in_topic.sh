ec4a3cbdb6044fab65c1b0c187e4160134d94eed
Abhinav Singh <abhinav.singh@specadel.com>
2016-10-27 14:56:08 +0530

    Removed '\n' from put_order_data_in_topic.sh

--- spark-streaming/kafka/put_order_data_in_topic.sh
+++ spark-streaming/kafka/put_order_data_in_topic.sh
@@ -4,7 +4,7 @@ export PATH=$PATH:/usr/hdp/current/kafka-broker/bin
 FILES=$1/*.csv
 for f in $FILES
 do
-    echo "\npushing $f file\n"
+    echo "pushing $f file"
     cat $f | kafka-console-producer.sh --broker-list $2  --topic $3
     sleep 60
 done
