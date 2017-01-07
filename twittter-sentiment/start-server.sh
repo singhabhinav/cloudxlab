28078af0b44d4e68c3a5bf80f31adb7f4f9bda42
Manjunath Kempaiah <manjunath.kmph@gmail.com>
2017-01-07 18:37:23 +0530

    Deleting this file

--- twittter-sentiment/start-server.sh
+++
@@ -1,8 +0,0 @@
-#script will install the node.js modules and start the server using script in package.json
-
-#command to install the node.js modules related to the project.
-#It refers the package.json to install the modules.
-npm install
-
-#Command to start the server by referring the package.json to start the script.
-npm start
